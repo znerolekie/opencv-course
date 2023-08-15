c30b48647d320d682dee13098abbeb17da8333f5
znerolekie <lorenz@iph-hannover.de>
2023-08-15 15:14:10 +0200

    added comments

--- Section #2 - Advanced/colour_spaces.py
+++ Section #2 - Advanced/colour_spaces.py
@@ -9,22 +9,28 @@ cv.imshow('Park', img)
 # plt.imshow(img)
 # plt.show()
 
+# BGR is the default color format in opencv
 # BGR to Grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Gray', gray)
 
+# Hue Saturation Value is based on the human perception of color
 # BGR to HSV
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 cv.imshow('HSV', hsv)
 
+# L*A*B
 # BGR to L*a*b
 lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
 cv.imshow('LAB', lab)
 
+# outside of opencv RGB format is the default color space
+# matplotlib would for example display a opencv-imag with inverted colors since the opencv BGR image is read as RGB
 # BGR to RGB
 rgb = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 cv.imshow('RGB', rgb)
 
+# It is not possible to convert from grayscale to HSV directly.
 # HSV to BGR
 lab_bgr = cv.cvtColor(lab, cv.COLOR_LAB2BGR)
 cv.imshow('LAB --> BGR', lab_bgr)
